5c4042c7864ca98ad8e89759b835f2725f0e67f4
Clemens Schmid <clemens@nevrome.de>
2018-11-19 12:39:00 +0100

    small changes in build procedure

--- _build.sh
+++ _build.sh
@@ -2,7 +2,5 @@
 
 bookdown::render_book(
   "index.Rmd",
-  output_file = "rendered_article/article.pdf",
-  output_dir = "rendered_article",
-  output_options = list(delete_merged_file = TRUE)
+  output_dir = "rendered_article"
 )
