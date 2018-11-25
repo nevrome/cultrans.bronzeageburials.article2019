73bc84a210e42fa784770a6fc9ef7417a5a4edb0
nevrome <clemens@nevrome.de>
2018-11-26 00:58:10 +0100

    first draft of dynamic rstudio spellchecking function -- does not work correctly yet

---
+++ spellcheck.R
@@ -0,0 +1,40 @@
+dynspell <- function() {
+
+  context <- rstudioapi::getSourceEditorContext()
+
+  range.start.row <- as.numeric(unlist(context$selection)["range.start.row"])
+  range.start.column <- as.numeric(unlist(context$selection)["range.start.column"])
+  range.end.row <- as.numeric(unlist(context$selection)["range.end.row"])
+  text <- as.character(unlist(context$selection)["text"])
+
+  rows <- range.start.row:range.end.row
+  start_columns <- c(range.start.column, rep(1, length(rows) - 1))
+  row_texts <- unlist(strsplit(text, "\n"))
+
+  range <- list()
+  i <- 1
+  for (p1 in 1:length(rows)) {
+    potentially_wrong_words <- unlist(hunspell::hunspell(row_texts[p1]))
+    if (length(potentially_wrong_words) == 0) { next }
+    positions <- stringr::str_locate_all(row_texts[p1], potentially_wrong_words)
+    for (p2 in 1:length(positions)) {
+      start <- rstudioapi::document_position(
+        row = rows[p1],
+        column = (start_columns[p1] + positions[[p2]][1]) - 1
+      )
+      end <- rstudioapi::document_position(
+        row = rows[p1],
+        column = start_columns[p1] + positions[[p2]][2]
+      )
+      range[[i]] <- rstudioapi::document_range(start, end)
+      i <- i + 1
+    }
+  }
+
+  rstudioapi::setSelectionRanges(
+    range,
+    id = context$id
+  )
+
+}
+
