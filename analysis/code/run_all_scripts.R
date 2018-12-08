bbbe37a25bec7f2866a3836e4ece35d74776cb4b
nevrome <clemens@nevrome.de>
2018-12-08 17:11:36 +0100

    real run script

--- analysis/code/run_all_scripts.R
+++ analysis/code/run_all_scripts.R
@@ -1,1 +1,20 @@
-cat("01\n")
+library(magrittr)
+
+lapply(
+  c(
+    "analysis/code/01_data_preparation_scripts",
+    "analysis/code/02_analysis_scripts",
+    "analysis/code/03_simulation_scripts",
+    "analysis/code/04_plot_scripts"
+  ),
+  function(x) {
+    pbapply::pblapply(
+      list.files(x, full.names = TRUE),
+      function(y) {
+        message("\n###### ", y, " ######\n")
+        source(y)
+        rm(list = ls())
+      }
+    )
+  }
+)
