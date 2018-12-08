e60398ebb5366752ba756cf9b02cd2d86198b6b2
nevrome <clemens@nevrome.de>
2018-12-08 12:14:04 +0100

    started to prepare repo for code and data

--- _build.sh
+++ _build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 rmarkdown::render(
-  "index.Rmd",
+  "article/index.Rmd",
   output_dir = "rendered_article",
   output_file = "article.pdf"
 )
--- _clone_required_repos.sh
+++
@@ -1,9 +0,0 @@
-git config --global user.email "clemens@nevrome.de"
-git config --global user.name "Clemens Schmid"
-
-cd ..
-
-git clone https://${GITHUB_PAT}@github.com/nevrome/neomod_analysis.git
-git clone https://${GITHUB_PAT}@github.com/nevrome/neiman1995.git
-
-cd neomod_textdev
