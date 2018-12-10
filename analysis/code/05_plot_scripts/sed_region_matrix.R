48e6c736fc87347946ca5ae491c55f0a15df8330
Clemens Schmid <clemens@nevrome.de>
2018-12-10 17:43:32 +0100

    basic layout of sed region matrix code

---
+++ analysis/code/05_plot_scripts/sed_region_matrix.R
@@ -0,0 +1,101 @@
+library(ggplot2)
+library(magrittr)
+library(GGally)
+
+load("analysis/data/tmp_data/squared_euclidian_distance_over_time_burial_construction.RData")
+regions_grid_burial_construction <- regions_grid
+regions_grid_burial_construction$variable <- "burial_construction"
+load("analysis/data/tmp_data/squared_euclidian_distance_over_time_burial_type.RData")
+regions_grid_burial_type <- regions_grid
+regions_grid_burial_type$variable <- "burial_type"
+load("analysis/data/tmp_data/region_colors.RData")
+load("analysis/data/tmp_data/region_order.RData")
+
+regions_grid <- rbind(regions_grid_burial_construction, regions_grid_burial_type)
+
+plot_list <- list()
+combinations <- c()
+iter <- 1
+for (row_region in region_order) {
+  for (col_region in region_order) {
+
+    if (row_region == col_region) {
+      plot_list[[iter]] <- ggally_text("")
+    } else {
+
+      second_run <- paste(row_region, col_region, sep = ".") %in% combinations
+      combinations[[iter]] <- paste(col_region, row_region, sep = ".")
+
+      if (!second_run) {
+        regions_grid_subset <- regions_grid %>%
+          dplyr::filter(
+            variable == "burial_construction",
+            regionA == col_region,
+            regionB == row_region
+          )
+        plot_starter <- regions_grid_subset %>%
+          ggplot()
+      } else {
+        regions_grid_subset <- regions_grid %>%
+          dplyr::filter(
+            variable == "burial_type",
+            regionA == col_region,
+            regionB == row_region
+          )
+        plot_starter <- regions_grid_subset %>%
+          ggplot()
+      }
+
+      plot_list[[iter]] <- plot_starter +
+        geom_line(
+          aes(time, sed, group = variable),
+          alpha = 0.3,
+          size = 0.5
+        ) +
+        geom_smooth(
+          aes(time, sed, group = variable, linetype = variable),
+          method = "loess",
+          span = 0.3,
+          size = 1.5
+        ) +
+        scale_x_continuous(
+          breaks = c(-2000, -1500, -1000),
+          limits = c(-2200, -800)
+        ) +
+        scale_y_continuous(
+          limits = c(0, 2)
+        ) +
+        theme_bw() +
+        theme(
+          axis.text = element_text(size = 10),
+          axis.text.x = element_text(angle = 45, hjust = 1),
+          axis.title = element_text(size = 15),
+          strip.text = element_text(size = 9),
+          legend.position = "bottom"
+        ) +
+        ylab("Squared Euclidian Distance") +
+        xlab("Time")
+
+    }
+
+    iter <- iter + 1
+  }
+}
+
+sed_matrix <- ggmatrix(
+  plot_list,
+  nrow = 8, ncol = 8,
+  byrow = FALSE
+) + theme_bw()
+
+
+sed_matrix %>%
+  ggsave(
+    "analysis/figures/sed_region_matrix.jpeg",
+    plot = .,
+    device = "jpeg",
+    scale = 1,
+    dpi = 300,
+    width = 300, height = 300, units = "mm",
+    limitsize = F
+  )
