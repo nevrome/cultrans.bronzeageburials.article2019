3ea7a685fedb1dae3c97547dafa0000a3aa9db63
nevrome <clemens@nevrome.de>
2019-05-24 15:49:06 +0200

    added additional plot to figure 6 with the median amount of graves per region

--- analysis/code/04_plot_scripts/correlation_time_series.R
+++ analysis/code/04_plot_scripts/correlation_time_series.R
@@ -1,7 +1,121 @@
 library(magrittr)
 library(ggplot2)
+library(cowplot)
 
-#### load and prepare data ###
+#### load and prepare time series data ###
+
+load("analysis/data/tmp_data/development_amount_burial_construction.RData")
+load("analysis/data/tmp_data/development_amount_burial_type.RData")
+
+data_amount_range_burial_construction <- amount_development_burial_construction %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(
+    timestep = -timestep
+  ) %>%
+  dplyr::filter(
+    idea != "unknown"
+  ) %>%
+  dplyr::group_by(region, timestep) %>%
+  dplyr::summarise(
+    sum_n = sum(n)
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::group_by(timestep) %>%
+  dplyr::summarise(
+    min_n = min(sum_n),
+    max_n = max(sum_n),
+    median_n = median(sum_n),
+    first_quartile_n = quantile(sum_n)[2],
+    third_quartile_n = quantile(sum_n)[4]
+  )
+
+data_amount_range_burial_type <- amount_development_burial_type %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(
+    timestep = -timestep
+  ) %>%
+  dplyr::filter(
+    idea != "unknown"
+  ) %>%
+  dplyr::group_by(region, timestep) %>%
+  dplyr::summarise(
+    sum_n = sum(n)
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::group_by(timestep) %>%
+  dplyr::summarise(
+    min_n = min(sum_n),
+    max_n = max(sum_n),
+    median_n = median(sum_n),
+    first_quartile_n = quantile(sum_n)[2],
+    third_quartile_n = quantile(sum_n)[4]
+  )
+
+
+
+#### data amount plot ####
+
+data_amount_plot <- ggplot() +
+  geom_ribbon(
+    data = data_amount_range_burial_type,
+    mapping = aes(
+      x = timestep,
+      ymin = first_quartile_n,
+      ymax = third_quartile_n
+    ),
+    fill = "#0072B2",
+    alpha = 0.25
+  ) +
+  geom_ribbon(
+    data = data_amount_range_burial_construction,
+    mapping = aes(
+      x = timestep,
+      ymin = first_quartile_n,
+      ymax = third_quartile_n
+    ),
+    fill = "#009E73",
+    alpha = 0.25
+  ) +
+  geom_line(
+    data = data_amount_range_burial_type,
+    mapping = aes(
+      x = timestep,
+      y = median_n
+    ),
+    colour = "#0072B2",
+    size = 1.5
+  ) +
+  geom_line(
+    data = data_amount_range_burial_construction,
+    mapping = aes(
+      x = timestep,
+      y = median_n
+    ),
+    linetype = "dashed",
+    colour = "#009E73",
+    size = 1.5
+  ) +
+  theme_bw() +
+  ylab("Graves per region") +
+  xlab("") +
+  scale_x_reverse(
+    limits = c(2200, 800), expand = c(0, 0),
+    position = "top",
+    breaks = seq(2000, 1000, -200),
+    minor_breaks = NULL
+  )  +
+  theme(
+    axis.text = element_text(size = 25),
+    axis.text.x = element_text(angle = 45, hjust = 0),
+    axis.title = element_text(size = 25),
+    plot.title = element_text(size = 25, face = "bold"),
+    panel.border = element_rect(colour = "black", size = 2),
+    plot.margin = unit(c(1,1,1,2), "lines")
+  )
+
+
+
+#### load and prepare correlation data ###
 
 load("analysis/data/tmp_data/mantel_sed_spatial_burial_type.RData")
 mantel_burial_type_spatial <- mantel_test_results
@@ -42,8 +156,9 @@ mantel$time <- factor(
   levels = gsub("-(?=[0-9])", "", levels(mantel$time), perl = TRUE)
 )
 
-# establish significance classes and create subsets only with significant results
+#### statistical significance ####
 
+# establish significance classes and create subsets only with significant results
 mantel <- mantel %>%
   dplyr::mutate(
     signif_class = cut(
@@ -62,8 +177,10 @@ signifs_0.1 <- mantel %>% dplyr::filter(
   signif_class == "p < 0.1"
 )
 
-#### plot ####
-p <- ggplot() +
+
+
+#### correlation time series plot ####
+correlation_time_series_plot <- ggplot() +
   geom_hline(
     yintercept = 0,
     colour = "red",
@@ -174,12 +291,25 @@ p <- ggplot() +
   ylab("Correlation coefficient") +
   xlab("Time steps in years calBC")
 
+#### combine plots ####
+
+combined_plot <- plot_grid(
+  data_amount_plot,
+  correlation_time_series_plot,
+  nrow = 2,
+  rel_heights = c(1, 3),
+  align = "v",
+  label_size = 35,
+  vjust = 1.0,
+  hjust = 0
+)
+
 ggsave(
   "analysis/figures/correlation_time_series.jpeg",
-  plot = p,
+  plot = combined_plot,
   device = "jpeg",
   scale = 1,
   dpi = 300,
-  width = 300, height = 280, units = "mm",
+  width = 300, height = 400, units = "mm",
   limitsize = F
 )
